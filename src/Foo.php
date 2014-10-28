9fbbc5b3b7ea43e77868569472e1d70214dbc02f
Jeandre Rohlandt <jrohlandt@gmail.com>
2014-10-28 11:09:58 +0200

    PHP Unit and Other Testing, Initial Commit

---
+++ src/Foo.php
@@ -0,0 +1,9 @@
+<?php namespace Acme;
+
+class Foo {
+
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ tests/FooTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class FooTest extends PHPUnit_Framework_TestCase {
+
+    public function test_it_works()
+    {
+        new Acme\Foo;
+    }
+}
